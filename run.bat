291ff58de08c20402047ad07cecd75c749a0f5c1
bill <bill@standke.us>
2026-09-17 10:35:38 -0500

    run scripts in batch file

---
+++ run.bat
@@ -0,0 +1,9 @@
+python.exe sortphotos2.py -s z:\2017 -d f:\Pictures
+python.exe sortphotos2.py -s z:\2018 -d f:\Pictures
+python.exe sortphotos2.py -s z:\2019 -d f:\Pictures
+rem python.exe sortphotos2.py -s z:\2020 -d f:\Pictures
+rem python.exe sortphotos2.py -s z:\2012 -d f:\Pictures
+rem python.exe sortphotos2.py -s z:\2013 -d f:\Pictures
+rem python.exe sortphotos2.py -s z:\2014 -d f:\Pictures
+rem python.exe sortphotos2.py -s z:\2015 -d f:\Pictures
+rem python.exe sortphotos2.py -s z:\2016 -d f:\Pictures
